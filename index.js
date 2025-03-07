20d3905e7f0574fa909ab91ed8edca6b3c3f9612
Ibrahima Dione <ibrahima.dione8@unchk.edu.sn>
2025-03-07 19:50:32 +0000

    premier phase

---
+++ index.js
@@ -0,0 +1,44 @@
+const questions=[
+    {
+        question :" Quel est le langage utilisé pour styliser une page web ?",
+        reponses: [
+            { text: "Python", correcte: false},
+            { text: "Java", correcte: false},
+            { text: "HTML", correcte: false},
+            { text: "CSS", correcte: true},
+            { text: "C++", correcte: false},
+        ]
+    },
+    {
+        question :"  Quel est le système d’exploitation open-source le plus utilisé pour les serveurs ?",
+        reponses: [
+            { text: "Windows", correcte: false},
+            { text: "macOS", correcte: false},
+            { text: "Linux ", correcte: true},
+            { text: "Android", correcte: false},
+            { text: "MS-DOS", correcte: false},
+        ]
+    },
+    {
+        question :"Quel est le résultat de l’opération 5 × 8 ?",
+        reponses: [
+            { text: "35", correcte: false},
+            { text: "45", correcte: false},
+            { text: "40 ", correcte: true},
+            { text: "30", correcte: false},
+            { text: "50", correcte: false},
+        ]
+    },
+    {
+        question :" Quelle planète est la plus proche du Soleil ?",
+        reponses: [
+            { text: "Vénus", correcte: false},
+            { text: "Terre", correcte: false},
+            { text: "Jupiter", correcte: false},
+            { text: "Mercure ", correcte: true},
+            { text: "Mars", correcte: false},
+        ]
+    }
+
+];
+
